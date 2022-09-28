ebb35919b304d8bb0b4000d56da4ee8ef56cab30
Umakant Kumar <umakantkumar@gmail.com>
2022-09-28 16:55:14 +0530

    First commit

---
+++ src/main/java/com/umakant/App.java
@@ -0,0 +1,15 @@
+package com.umakant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.print("\033[H\033[2J");
+        System.out.println( "This is Umakant Kumar.\nTesting maven." );
+        System.out.flush();
+    }
+}
