5e740549cf56b8951be5541d4afea24e7c07c6df
Umakant Kumar <umakantkumar@gmail.com>
2022-09-29 22:50:28 +0530

    Code change 4th

--- src/main/java/com/umakant/App.java
+++ src/main/java/com/umakant/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "This is Umakant Kumar.\nTesting Maven-Project.\nBuild, Deploy & Test." );
+        System.out.println("email: umakantkumar@gmail.com\nContact: 7042277549");
         System.out.flush();
     }
 }
