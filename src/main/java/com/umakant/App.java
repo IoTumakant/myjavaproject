110a87316fd8a02f333341edc8b4e0b042afa207
Umakant Kumar <umakantkumar@gmail.com>
2022-09-30 07:53:41 +0530

    Code change 6th

--- src/main/java/com/umakant/App.java
+++ src/main/java/com/umakant/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "This is Umakant Kumar.\nTesting Maven-Project.\nBuild, Deploy & Test." );
         System.out.println("We are using poll SCM for  build trigger.");
-        System.out.println("email: umakantkumar@gmail.com\nContact: 7042277549");
+        System.out.println("email: umakantkumar@gmail.com\nContact: 7042277549\nAdderss: Villa 334, The Emperian, Chikaturupati");
         System.out.flush();
     }
 }
