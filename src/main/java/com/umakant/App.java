38e23c56cc0c3423198a003f8e542b6b8ce4c763
Umakant Kumar <umakantkumar@gmail.com>
2022-10-12 14:53:09 +0530

    Code change 11th

--- src/main/java/com/umakant/App.java
+++ src/main/java/com/umakant/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Testing Maven-Project.\nBuild, Deploy & Test." );
         System.out.println("We are using poll SCM for  build trigger.");
         System.out.println("Name: Raju Kumar\nemail: rajukumar@gmail.com\nContact: 7082277549\nAdderss: Villa 334, The Emperian, Chikaturupati");
+        System.out.println("Merge test to Master.")
         System.out.flush();
     }
 }
